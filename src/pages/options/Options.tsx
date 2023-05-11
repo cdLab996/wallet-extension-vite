56a9d09aac4b9d3f2f6fc45b21c447c058aaa965
wudi <wuchendi96@gmail.com>
2023-05-11 10:51:15 +0800

    feat: first commit

---
+++ src/pages/options/Options.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "@pages/options/Options.css";
+
+const Options: React.FC = () => {
+  return <div className="OptionsContainer">Options 123</div>;
+};
+
+export default Options;
---
+++ src/pages/panel/Panel.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "@pages/panel/Panel.css";
+
+const Panel: React.FC = () => {
+  return (
+    <div className="container">
+      <h1>Dev Tools Panel 123</h1>
+    </div>
+  );
+};
+
+export default Panel;
