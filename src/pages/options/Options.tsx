f8b0deb6a059176d3a8fd6e7ae5af59f09b75b05
wudi <wuchendi96@gmail.com>
2023-05-18 14:25:57 +0800

    feat: Remove test code, and unify the style file for newtab.

--- src/pages/options/Options.tsx
+++ src/pages/options/Options.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import '@pages/options/Options.css'
 
 const Options: React.FC = () => {
-  return <div className='OptionsContainer'>Options 123</div>
+  return <div className='OptionsContainer'>Options</div>
 }
 
 export default Options
